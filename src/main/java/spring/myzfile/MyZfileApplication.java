1f797459f35e859275be8beeda279f53dd34eca5
zhang <2806700577@qq.com>
2025-11-02 18:57:00 +0800

    Initial commit

---
+++ src/main/java/spring/myzfile/MyZfileApplication.java
@@ -0,0 +1,13 @@
+package spring.myzfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyZfileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyZfileApplication.class, args);
+    }
+
+}
---
+++ src/test/java/spring/myzfile/MyZfileApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.myzfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyZfileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
